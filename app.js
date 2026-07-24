faf28b77169a8effed943168154017565f1ba6df
sebastiansuns-hub <sunssebastian@gmail.com>
2026-07-24 14:44:01 -0500

    24 de julio, pruebas iniciales

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Aprendicez ficha 3407186');
+});
+
+app.listen(port, () => {
+  console.log(`servidor en funcionamiento al puerto:`);
+});
